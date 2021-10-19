84dcbaa88b680948a79b615088b1a8a90acb1e82
Aahana11873 <76442757+Aahana11873@users.noreply.github.com>
2021-10-19 13:15:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+noseX=0;
+noseY=0;
+rightwristX=0;
+leftwristX=0;
+difference=0;
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(500, 500);
+    canvas=createCanvas(550, 500);
+    canvas.position(560, 150)
+poseNet=ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log('posenet is initialised');
+}
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("noseX= "+noseX+"noseY "+noseY);
+        leftwristX=results[0].pose.leftWrist.x;
+        rightwristX=results[0].pose.rightWrist.x;
+        difference=floor(leftwristX-rightwristX);
+         console.log("leftwristX= "+leftwristX+"rightwristX "+rightwristX);
+    }
+}
+function draw(){
+    background('pink');
+    document.getElementById("square_side").innerHTML="width and height of the square will be "+difference+"px";
+fill("green");
+Text(Aahana, 100, 70)
+
+}
